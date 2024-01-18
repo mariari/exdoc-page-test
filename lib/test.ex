f7c786910b3d5d8a39fc950eb80dc5a821aa18b0
mariari <mariari@protonmail.com>
2024-01-18 13:47:26 +0800

    Add mermaid diagram

--- lib/test.ex
+++ lib/test.ex
@@ -1,6 +1,14 @@
 defmodule Test do
   @moduledoc """
   Documentation for `Test`.
+
+  ```mermaid
+  graph BT
+      C(Client #3) ~~~ B(Client #2) ~~~ A(Client #1)
+      A & B & C -->|request| GenServer
+      GenServer -.->|reply| A & B & C
+  ```
+
   """
 
   @doc """
