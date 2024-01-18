109921838023cbbcf89899adba71a0f1e0c52bf1
mariari <mariari@protonmail.com>
2024-01-18 14:45:38 +0800

    Fixup the naming for pre_built

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Test.MixProject do
   def project do
     [
       app: :foo,
-      version: "0.4.0",
+      version: "0.5.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
