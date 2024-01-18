ed70fd6292ac2474943ec3ea71c5a120be9a7315
mariari <mariari@protonmail.com>
2024-01-18 16:27:11 +0800

    Add Mermaid to the docs

--- mix.exs
+++ mix.exs
@@ -46,7 +46,8 @@ defmodule Foo.MixProject do
     [
       main: "readme",
       extras: extras(),
-      extra_section: "GUIDES"
+      extra_section: "GUIDES",
+      before_closing_body_tag: &docs_before_closing_body_tag/1
     ]
   end
 
@@ -56,5 +57,33 @@ defmodule Foo.MixProject do
 
   def escript do
     [main_module: Foo.CLI]
+  end
+
+  defp docs_before_closing_body_tag(:html) do
+    # https://hexdocs.pm/ex_doc/readme.html#extensions
+    """
+    <script src="https://cdn.jsdelivr.net/npm/mermaid@10.7.0/dist/mermaid.min.js"></script>
+    <script>
+    document.addEventListener("DOMContentLoaded", function () {
+    mermaid.initialize({
+      startOnLoad: false,
+      theme: document.body.className.includes("dark") ? "dark" : "default"
+    });
+    let id = 0;
+    for (const codeEl of document.querySelectorAll("pre code.mermaid")) {
+      const preEl = codeEl.parentElement;
+      const graphDefinition = codeEl.textContent;
+      const graphEl = document.createElement("div");
+      const graphId = "mermaid-graph-" + id++;
+      mermaid.render(graphId, graphDefinition).then(({svg, bindFunctions}) => {
+        graphEl.innerHTML = svg;
+        bindFunctions?.(graphEl);
+        preEl.insertAdjacentElement("afterend", graphEl);
+        preEl.remove();
+      });
+    }
+    });
+    </script>
+    """
   end
 end
